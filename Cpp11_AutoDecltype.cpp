7b5c528f2e7486022d1094e0c8c5cecd0b4c1c3f
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2023-04-20 17:43:39 +0200

    Extended the example for some more cases

--- Cpp11_AutoDecltype.cpp
+++ Cpp11_AutoDecltype.cpp
@@ -4,14 +4,15 @@
 
 /*
 
-    specifies that the type of the variable that is being declared will be automatically deduced from its initializer
+    specifies that the data type is being automaticaly deduced from the initializer
 
+    - C++ 11 for variables, specifies that the return type will be deduce from its return statements
     - C++ 14 for functions, specifies that the return type will be deduced from its return statements
     - C++ 17 for non-type template parameters, specifies that the type will be deduced from the argument
 
     syntax:
     <type-constraint> auto              : type is deduced using the rules for template argument deduction
-    <type-constraint> decltype(auto)    : type is deduced from the expression as initializer
+    <type-constraint> decltype(auto)    : type is deduced from decltype(expresion) the expression is used as initializer
     
 */
 
@@ -32,11 +33,22 @@ namespace{
             std::cout << "copy-constructor called" << std::endl;
         }
     };
-    
+   
     template<typename To, typename From>
     To convert(From f){
         std::cout << "To data type is: " << typeid(f).name() << std::endl;
         return static_cast<To>(f);
+    }
+
+    template<typename T, auto cnt>
+    void deduceFromNonTypeTemplateParameter()
+    {
+        std::cout << "Non-type parameter deduced: " << typeid(cnt).name() << std::endl;
+        T arr[cnt];
+        for (auto item:arr)
+        {
+            std::cout << "Item=" << item << std::endl;
+        }
     }
 
     double getDouble(){
@@ -65,10 +77,11 @@ namespace{
 /*
     Template Argument Deduction:
     in order to instantiate a function template, every template argument must be knows, but not every template argument
-    has to be specified. When possible, the compiler will deduce the missing template arguments from the function arguments. 
+    has to be specified. 
+    When possible, the compiler will deduce the missing template arguments from the function arguments. 
     This occurs when a function call is attempted, when an address of a function template is taken, and in some other contexts
 
-    This mechanism makes it possible to use template operators, since there is no syntax to define template arguments for an operator
+    This mechanism makes it possible to use template operators, since there is no syntax to specify template arguments for an operator
 */
 
 void Cpp11_AutoDecltype::auto_TemplateArgumentDeduction()
@@ -77,30 +90,41 @@ void Cpp11_AutoDecltype::auto_TemplateArgumentDeduction()
     double argument{0.0};
 
     // examples of the type deductions from input arguments
-    auto i = convert<int>(d);
+    int i = convert<int>(d);
     i = convert<int, double>(3);                    // both arguments are known and specified -> no deduction is needed
     i = convert<int>(3);                            // first argument is specified and second argument is just known
     i = convert<int>(argument);                     // first argument is specified and second argument is deduced from variable
-      int (*ptr)(double) = convert<int, double>;    // both arguments are known and specified
+    int (*ptr)(double) = convert<int, double>;      // both arguments are known and specified
     int (*ptr1)(double) = convert<int>;             // first argument is specified and second argument is deduces from function poiter parameter
     double (*ptr2)(int) = convert<double>;          // the new function address is assigned as the argument types has changed
     deduceFromInitList({1, 2, 3});                  // the type is deduced from the items data types, but must be the same type
     deduceFromInitListSize({1, 2, 3, 4});           // the type is deduced from the items data types and the size too
+    deduceFromNonTypeTemplateParameter<int, 5>();   // the type is deduced from the constexpr in non-type template parameter
  
     std::cout << "This text will be used for deduction" << std::endl;  // template argument deduction makes possible to deduce argument type for operators
 
     // exmaples of deduction from the function calls
     i = convert<int>(getDouble());                  // first argument is specified and second argument is deduced from function
+
+    // C++ 17 type deduction in structured binding
+    int a[2] = {1, 2};
+    auto [x, y] = a;        // creates e[2], copies a into e
+    x = 123;                // then x refers to e[0], y refers to e[1]
+    auto& [xr, yr] = a;     // xr refers to a[0], yr referes to a[1]
+    xr =  123;              // will change a[0]
 }
 
 void Cpp11_AutoDecltype::auto_SimpleTypeDeduction()
 {
-    auto auto_variable{0};
-    static_assert(std::is_same<decltype(auto_variable), int>::value, "variable does not have expected type");
+    // ill-formed exmaple
+    auto auto_variable0 = 1; //, auto_variable_IllFormed = 1.0;
 
-    int type_variable1{0};
-    auto auto_variable1 {type_variable1};
+    auto auto_variable1{0};
     static_assert(std::is_same<decltype(auto_variable1), int>::value, "variable does not have expected type");
+
+    int type_variable2{0};
+    auto auto_variable2 {type_variable2};
+    static_assert(std::is_same<decltype(auto_variable2), int>::value, "variable does not have expected type");
 }
 
 void Cpp11_AutoDecltype::auto_ConstTypeDeduction()
