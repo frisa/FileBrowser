ffa10a72db878165d81f6e300c4f8d0458d986b7
Jan Fridrichovsky <fridrichovsky@gmail.com>
2023-04-23 07:34:14 +0200

    Code cleanup

--- src/program.cpp
+++ src/program.cpp
@@ -8,7 +8,3 @@ int main(int argc, char **argv)
     engine.load(QUrl::fromLocalFile("../qml/main.qml"));
     return app.exec();
 }
-
-// some notes
-//https://lightrun.com/solutions/3fon3fonov-exostriker-qtqpaplugin-could-not-load-the-qt-platform-plugin-xcb-in-even-though-it-was-found-ubuntu-r/
-// sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
